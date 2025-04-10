6c9488c58c285b59045970deaea79890feebc2d7
Truman Mulholland <truman.mulholland@gmail.com>
2025-04-11 10:12:32 +1200

    Fix transformer inheritance

--- dynamo/useful.py
+++ dynamo/useful.py
@@ -1,100 +1,17 @@
 from __future__ import annotations
 
 import logging
-import re
 from functools import partial
 
 import discord
-from async_utils.lru import LRU
-from discord import AppCommandOptionType, app_commands
+from discord import app_commands
 from discord.app_commands import Transform
 
 from .bot import BotExports, Interaction
 from .utils.logic import process_async_iterable
-from .utils.transformers import DynamoTransformer
-
-_guild_events_cache: LRU[int, list[discord.ScheduledEvent]] = LRU(128)
+from .utils.transformers import ScheduledEventTransformer
 
 log = logging.getLogger(__name__)
-
-
-class ScheduledEventTransformer(DynamoTransformer):
-    """discord.ScheduledEvent transformer adapted from the discord.py converter.
-
-    Lookups are done for the local guild if available. Otherwise, for a DM context,
-    lookup is done by the global cache.
-
-    The lookup strategy is as follows (in order):
-
-    1. Lookup by ID.
-    2. Lookup by url.
-    3. Lookup by name.
-    """
-
-    @staticmethod
-    def _get_cached(
-        values: list[discord.ScheduledEvent], value: str, /
-    ) -> discord.ScheduledEvent:
-        return next(
-            e
-            for e in values
-            if e.name.casefold() == value.casefold()
-            or str(e.id) == value
-            or e.url.lower() == value.lower()
-        )
-
-    async def transform(self, itx: Interaction, value: str, /) -> discord.ScheduledEvent:
-        if itx.guild is None:
-            msg = "Tried transforming event outside of guild"
-            raise app_commands.NoPrivateMessage(msg) from None
-
-        guild: discord.Guild | None = itx.guild
-        events = _guild_events_cache.setdefault(guild.id, [])
-        result: discord.ScheduledEvent | None = None
-        try:
-            result = ScheduledEventTransformer._get_cached(events, value)
-        except StopIteration:
-            log.debug("%s is not yet cached for guild %d", value, guild.id)
-        else:
-            log.debug("%s is already cached for guild %d.", value, guild.id)
-            return result
-
-        match = DynamoTransformer._get_id_match(value)
-
-        if match:
-            # ID match
-            event_id = int(match.group(1))
-            result = guild.get_scheduled_event(event_id)
-        else:
-            pattern = (
-                r"https?://(?:(ptb|canary|www)\.)?discord\.com/events/"
-                r"(?P<guild_id>[0-9]{15,20})/"
-                r"(?P<event_id>[0-9]{15,20})$"
-            )
-            match = re.match(pattern, value, flags=re.IGNORECASE)
-            if match:
-                # URL match
-                guild = itx.client.get_guild(int(match.group("guild_id")))
-
-                if guild is not None:
-                    event_id = int(match.group("event_id"))
-                    result = guild.get_scheduled_event(event_id)
-            # lookup by name
-            else:
-                result = discord.utils.get(guild.scheduled_events, name=value)
-        if result is None:
-            raise app_commands.TransformerError(value, self.type, self) from None
-
-        if not events:
-            _guild_events_cache[itx.guild.id] = [result]
-        else:
-            _guild_events_cache[itx.guild.id].append(result)
-
-        return result
-
-    @property
-    def type(self) -> AppCommandOptionType:
-        return AppCommandOptionType.string
 
 
 @app_commands.command(
--- dynamo/utils/transformers.py
+++ dynamo/utils/transformers.py
@@ -1,24 +1,110 @@
 from __future__ import annotations
 
+import logging
 import re
+from collections.abc import Generator
 
+import discord
+from async_utils.lru import LRU
+from discord import AppCommandOptionType, app_commands
 from discord.app_commands import Transformer
+
+from dynamo import _typings as t
+from dynamo.bot import Interaction
 
 _ID_REGEX = re.compile(r"([0-9]{15,20})$")
 
+log = logging.getLogger(__name__)
 
-class DynamoTransformer(Transformer["Dynamo"]):  # type: ignore[reportUnknownVariable]
-    """Base class for transformers that are used in the Dynamo library.
 
-    This class is a subclass of `app_commands.Transformer` and provides a
-    common interface for all transformers in the library. It also provides
-    some utility methods for working with transformers.
+def _get_id_match(value: str) -> re.Match[str] | None:
+    return _ID_REGEX.match(value)
+
+
+_guild_events_cache: LRU[int, list[discord.ScheduledEvent]] = LRU(128)
+
+
+def _get_cached_event(
+    values: list[discord.ScheduledEvent],
+    value: str,
+    /,
+) -> Generator[discord.ScheduledEvent]:
+    return (
+        e
+        for e in values
+        if e.name.casefold() == value.casefold()
+        or str(e.id) == value
+        or e.url.lower() == value.lower()
+    )
+
+
+class DynamoTransformer(Transformer):
+    async def transform(self, itx: Interaction, value: t.Any, /) -> t.Any:  # type: ignore[reportIncompatibleMethodOverride]  # noqa: PLR6301
+        return NotImplemented
+
+
+class ScheduledEventTransformer(DynamoTransformer):
+    """discord.ScheduledEvent transformer adapted from the discord.py converter.
+
+    Lookups are done for the local guild if available. Otherwise, for a DM context,
+    lookup is done by the global cache.
+
+    The lookup strategy is as follows (in order):
+
+    1. Lookup by ID.
+    2. Lookup by url.
+    3. Lookup by name.
     """
 
-    @staticmethod
-    def _get_id_match(value: str) -> re.Match[str] | None:
-        return _ID_REGEX.match(value)
+    async def transform(self, itx: Interaction, value: str, /) -> discord.ScheduledEvent:
+        if itx.guild is None:
+            msg = "Tried transforming event outside of guild"
+            raise app_commands.NoPrivateMessage(msg) from None
 
-    @staticmethod
-    def _get_cached[T](values: list[T], value: str, /) -> T:
-        return NotImplemented
+        guild: discord.Guild | None = itx.guild
+        events = _guild_events_cache.setdefault(guild.id, [])
+        result: discord.ScheduledEvent | None = None
+        try:
+            result = next(_get_cached_event(events, value))
+        except StopIteration:
+            log.debug("%s is not yet cached for guild %d", value, guild.id)
+        else:
+            log.debug("%s is already cached for guild %d.", value, guild.id)
+            return result
+
+        match = _get_id_match(value)
+
+        if match:
+            # ID match
+            event_id = int(match.group(1))
+            result = guild.get_scheduled_event(event_id)
+        else:
+            pattern = (
+                r"https?://(?:(ptb|canary|www)\.)?discord\.com/events/"
+                r"(?P<guild_id>[0-9]{15,20})/"
+                r"(?P<event_id>[0-9]{15,20})$"
+            )
+            match = re.match(pattern, value, flags=re.IGNORECASE)
+            if match:
+                # URL match
+                guild = itx.client.get_guild(int(match.group("guild_id")))
+
+                if guild is not None:
+                    event_id = int(match.group("event_id"))
+                    result = guild.get_scheduled_event(event_id)
+            # lookup by name
+            else:
+                result = discord.utils.get(guild.scheduled_events, name=value)
+        if result is None:
+            raise app_commands.TransformerError(value, self.type, self) from None
+
+        if not events:
+            _guild_events_cache[itx.guild.id] = [result]
+        else:
+            _guild_events_cache[itx.guild.id].append(result)
+
+        return result
+
+    @property
+    def type(self) -> AppCommandOptionType:
+        return AppCommandOptionType.string
